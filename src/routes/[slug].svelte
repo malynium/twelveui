39f4b52056ff6f74787387ff5a3445070b293255
Jason Creviston <jcreviston@pm.me>
2022-01-12 13:38:14 -0500

    adds stuff

--- src/routes/[slug].svelte
+++ src/routes/[slug].svelte
@@ -2,4 +2,4 @@
   import { page } from '$app/stores'
   $: slug = $page.params.slug
 </script>
-<h1 class="twelveui-text-3xl">Welcome to {slug}!</h1>
+<h1 class="text-3xl text-center">Welcome to {slug}!</h1>
